fac5cdf8d2afb464f72d809eac7bd10fb6c65106
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-06-05 11:43:17 +0530

    Update deploySite.sh

--- deploySite.sh
+++ deploySite.sh
@@ -17,6 +17,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# 
+# Helluva Neu DataElementé : marital_status_single
 #
 
 mvn -Preporting site site:stage "$@"
